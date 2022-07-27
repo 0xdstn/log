d4814a0d59c628b085ceb2c3145c636bbc96a264
Dustin <0xdstn@protonmail.com>
2022-07-27 00:04:21 +0000

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# LOG
+
+I have used the official Bullet Journal Companion app for a long time now, and I have always wished I could turn off the 72 hour expiration of entries.
+
+I know the intent with the 72 hour limit is to push you to transfer to your journal every day, but my life doesn't always lend to that.
+
+You can add/edit notes, tasks, and events, and delete them when you're ready.
+
+This site is a PWA, meaning you can install it to your device in Android and it will look and act like a native application.
+
+Live version: [http://tilde.town/~dustin/log/](http://tilde.town/~dustin/log/)
---
+++ main.js
@@ -0,0 +1,104 @@
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js');
+};
+
+const body = document.getElementById('body');
+const about = document.getElementById('about');
+const entriesRender = document.getElementById('entries');
+const openAbout = document.getElementById('open-about');
+const closeAbout = document.getElementById('close-about');
+const entryInput = document.getElementById('entry');
+const glyphInput = document.getElementById('glyph');
+const saveButton = document.getElementById('save');
+
+const glyphs = {
+    '-' : 'note',
+    '.' : 'task',
+    'o' : 'event',
+    'x' : 'done'
+};
+
+var editing = false;
+var editingIndex = null;
+
+var entries = '';
+var l = localStorage.getItem('log');
+if(l !== null) {
+    entries = l;
+}
+
+openAbout.addEventListener('click',function(e) {
+    e.preventDefault();
+    about.classList.add('open');
+    body.classList.add('modalOpen');
+});
+
+closeAbout.addEventListener('click',function(e) {
+    e.preventDefault();
+    about.classList.remove('open');
+    body.classList.remove('modalOpen');
+});
+
+saveButton.addEventListener('click',function(e) {
+    e.preventDefault();
+    var newEntry = `${glyphInput.value} ${entryInput.value}`;
+    if(editing) {
+        var items = entries.split('|');
+        items[editingIndex] = newEntry;
+        entries = items.join('|');
+        editing = false;
+        editingIndex = null;
+    } else {
+        var sep = '|';
+        if(entries == '') {
+            sep = '';
+        }
+        entries += sep + newEntry;
+    }
+    localStorage.setItem('log',entries);
+    entryInput.value = '';
+    renderLogs();
+});
+
+function renderLogs() {
+    var out = '';
+
+    if(entries.length) {
+        var items = entries.split('|');
+
+        items.forEach((i,index) => {
+            var glyph = i[0];
+            var entry = i.substring(2);
+            out += `<a href="#" data-action="del" data-i="${index}">D</a> <a href="#"  data-action="edit" data-i="${index}">E</a> <span class="${glyphs[glyph]}">${glyph}</span> ${entry}<br>`;
+        });
+    }
+
+    entriesRender.innerHTML = out;
+
+    document.querySelectorAll('#entries a').forEach(btn => {
+        btn.addEventListener('click',function(e){
+            e.preventDefault();
+            console.log(e.target);
+            var action = e.target.getAttribute('data-action');
+            var i = e.target.getAttribute('data-i');
+
+            if(action == 'del') {
+                var items = entries.split('|');
+                items.splice(i,1);
+                entries = items.join('|');
+                localStorage.setItem('log',entries);
+                renderLogs();
+            } else if(action == 'edit') {
+                var items = entries.split('|');
+                var item = items[i];
+                var glyph = item[0];
+                var entry = item.substring(2);
+                entryInput.value = entry;
+                editing = true;
+                editingIndex = parseInt(i);
+            }
+        });
+    });
+}
+
+renderLogs();
